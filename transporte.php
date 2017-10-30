bf3724727a92ae2a1f9eb789c69528145bce0f2c
mgrimblat <mgrimblat@live.com>
2017-10-30 00:30:23 -0300

    agrego monto a clase tpte.

--- transporte.php
+++ transporte.php
@@ -2,12 +2,15 @@
 
 class transporte{
 
-    protected $tipo;
-    protected $monto;
+  protected $tipo;
+  protected $monto;
 
-    public function get_tipo{
-        return $this->tipo;
-    }
+  public function get_tipo{
+    return $this->tipo;
+  }
+
+  public function get_monto{
+    return $this->monto;
+  }
 }
-
 ?>
