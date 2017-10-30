cdf36049ead6a3e12b754e6e3217f2f66a6d6c02
mgrimblat <mgrimblat@live.com>
2017-10-30 02:19:19 -0300

    agrego construct a viaje

--- viaje.php
+++ viaje.php
@@ -5,6 +5,13 @@
     protected $tipo;      //plus, normal, pase libre, etc.
     protected $monto;
     protected $transporte;
+
+    function __construct($tipo, $monto, $transporte){
+
+      $this->tipo = $tipo;
+      $this->monto = $monto;
+      $this->transporte = $transporte;
+    }
   }
 
  ?>
