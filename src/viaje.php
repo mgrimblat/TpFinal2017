124917a075a26e410fb830d66cc094f7d2f3723b
mgrimblat <mgrimblat@live.com>
2017-11-02 11:10:26 -0300

    Update viaje.php

--- src/viaje.php
+++ src/viaje.php
@@ -1,25 +1,25 @@
 <?php
 
-  class viaje{
+namespace TpFinal;
+class viaje{
 
-    protected $transbordo;      // booleano
-    protected $monto;
-    protected $transporte;
-    protected $fecha;
+ protected $transbordo;      // booleano
+ protected $monto;
+ protected $transporte;
+ protected $fecha;
 
-    function __construct($transbordo, $monto, $transporte, $fecha){
+ function __construct($transbordo, $monto, $transporte, $fecha){
 
-      $this->transbordo = transbordo;
-      $this->monto = $monto;
-      $this->transporte = $transporte;
-      $this->fecha = $fecha;
-    }
+  $this->transbordo = transbordo;
+  $this->monto = $monto;
+  $this->transporte = $transporte;
+  $this->fecha = $fecha;
+ }
 
-    function get_fecha{
+ function get_fecha{
 
-      return $this->fecha
-
-    }
+  return $this->fecha
   }
+}
 
  ?>
