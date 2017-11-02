86a395f0c241d062ec364cbe3904f4332c170a6c
mgrimblat <mgrimblat@live.com>
2017-11-02 11:58:34 -0300

    test cargar 50 pesos

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -7,9 +7,15 @@ use PHPUnit\Framework\TestCase;
 class TarjetaTest extends TestCase {
 
 
-    public function testSaldoCero() {
+    public function test_SaldoCero() {
         $tarjeta = new tarjeta;
         $this->assertEquals($tarjeta->get_saldo(), 0.0);
+    }
+    
+    public function test_cargar50() {
+        $tarjeta = new tarjeta;
+        $tarjeta->carga(50);
+        $this->assertEquals($tarjeta->get_saldo(), 50.0);
     }
 }
 
