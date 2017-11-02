2177139fc841df4d22c2f1b0174d64a3273a188a
mgrimblat <mgrimblat@live.com>
2017-11-02 12:02:44 -0300

    agrego test 332 pesos

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -14,9 +14,17 @@ class TarjetaTest extends TestCase {
     
     public function test_cargar50() {
         $tarjeta = new tarjeta;
-        $tarjeta->carga(50);
+        $tarjeta->carga(50.0);
         $this->assertEquals($tarjeta->get_saldo(), 50.0);
     }
+    
+    public function test_cargar332() {
+        $tarjeta = new tarjeta;
+        $tarjeta->carga(332.0);
+        $this->assertEquals($tarjeta->get_saldo(), 338.0);
+    }
+    
+    
 }
 
 ?>
