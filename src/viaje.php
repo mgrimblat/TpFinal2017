f2b3fd084964d8a10c83c05ff4deaa7a58c2c1a3
mgrimblat <mgrimblat@live.com>
2017-11-03 09:50:52 -0300

    agrego $id a la clase viaje

--- src/viaje.php
+++ src/viaje.php
@@ -7,13 +7,19 @@ class viaje{
  protected $monto;
  protected $transporte;
  protected $fecha;
+ protected $id; // 0 si es una bici, si es un colectivo el nro. de linea
 
- function __construct($transbordo, $monto, $transporte, $fecha){
+ function __construct($transbordo, $monto, $transporte, $id, $fecha){
 
   $this->transbordo = $transbordo;
   $this->monto = $monto;
   $this->transporte = $transporte;
   $this->fecha = $fecha;
+  $this->id = $id;
+ }
+
+ function get_id(){
+   return $this->id;
  }
 
  function get_fecha(){
