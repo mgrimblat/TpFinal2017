aad52a9e0c322c8610137dc29ba51bd34c450b89
Juancarreras97 <juancarreras97@hotmail.com>
2017-11-03 03:46:30 -0300

    Update TarjetaTest.php

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -26,7 +26,7 @@ class TarjetaTest extends TestCase {
     
        public function test_Pagar1Viaje(){
         $tarjeta = new tarjeta;
-        $tarjeta->carga(50);
+        $tarjeta->carga(50.0);
         $colectivo = new colectivo("K", "Naranja");
         $tarjeta->pagar_viaje($colectivo, "02/11/2017 12:34:20");
         $this->assertEquals($tarjeta->get_saldo(), 50-9.70);
