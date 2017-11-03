d0eb362ef845e76c62d85eb5c79a5f74288083f9
mgrimblat <mgrimblat@live.com>
2017-11-03 09:03:24 -0300

    borro test 1 viaje

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -35,13 +35,7 @@ class TarjetaTest extends TestCase {
 
 
 
-       public function test_Pagar1Viaje(){
-        $tarjeta = new tarjeta;
-        $tarjeta->carga(50.0);
-        $colectivo = new colectivo("K", "Naranja");
-        $tarjeta->pagar_viaje($colectivo, "02/11/2017 12:34:20");
-        $this->assertEquals($tarjeta->get_saldo(), 50.0 - 9.70);
-    }
+
 
 
 }
