eacfdea2049a7540fd4526c7fb32e71bef860559
mgrimblat <mgrimblat@live.com>
2017-11-05 22:16:39 -0300

    borro tabs

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -8,20 +8,20 @@ class TarjetaTest extends TestCase {
 
 
     public function test_SaldoCero() {
-        $tarjeta = new tarjeta;
-        $this->assertEquals($tarjeta->get_saldo(), 0.0);
+      $tarjeta = new tarjeta;
+      $this->assertEquals($tarjeta->get_saldo(), 0.0);
      }
 
     public function test_cargar50() {
-        $tarjeta = new tarjeta;
-        $tarjeta->carga(50.0);
-        $this->assertEquals($tarjeta->get_saldo(), 50.0);
+      $tarjeta = new tarjeta;
+      $tarjeta->carga(50.0);
+      $this->assertEquals($tarjeta->get_saldo(), 50.0);
     }
 
     public function test_cargar332() {
-        $tarjeta = new tarjeta;
-        $tarjeta->carga(332.0);
-        $this->assertEquals($tarjeta->get_saldo(), 388.0);
+      $tarjeta = new tarjeta;
+      $tarjeta->carga(332.0);
+      $this->assertEquals($tarjeta->get_saldo(), 388.0);
     }
 
     public function pagar_bici(){
