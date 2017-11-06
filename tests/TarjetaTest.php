8e6b8d9b044fa7d476279faf1ea03ebc5c8b2795
mgrimblat <mgrimblat@live.com>
2017-11-05 23:00:49 -0300

    update

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -24,23 +24,6 @@ class TarjetaTest extends TestCase {
       $this->assertEquals($tarjeta->get_saldo(), 388.0);
     }
 
-    public function pagar_bici(){
-      $tarjeta = new tarjeta;
-      $tarjeta->carga(50.0);
-      $bici = new bicicleta;
-      $tarjeta->pagar_viaje($bici, "03/11/2017 08:54");
-      $this->assertEquals($tarjeta->get_saldo(), 50 - 12.45);
-    }
-
-    public function pagar_2bici(){
-      $tarjeta = new tarjeta;
-      $tarjeta->carga(50.0);
-      $bici = new bicicleta;
-      $tarjeta->pagar_viaje($bici, "03/11/2017 08:54");
-      $tarjeta->pagar_viaje($bici, "03/11/2017 08:57");
-      $this->assertEquals($tarjeta->get_saldo(), 50 - (12.45 * 2));
-    }
-
     public function test_Pagar1Viaje(){
       $tarjeta = new tarjeta;
       $tarjeta->carga(50.0);
@@ -86,7 +69,22 @@ class TarjetaTest extends TestCase {
       $this->assertEquals($tarjeta->get_saldo(), 50.0 - ((9.70 + 2.91)/2));
     }
 
+    public function pagar_bici(){
+      $tarjeta = new tarjeta;
+      $tarjeta->carga(50.0);
+      $bici = new bicicleta;
+      $tarjeta->pagar_viaje($bici, "03/11/2017 08:54");
+      $this->assertEquals($tarjeta->get_saldo(), 50 - 12.45);
+    }
 
+    public function pagar_2bici(){
+      $tarjeta = new tarjeta;
+      $tarjeta->carga(50.0);
+      $bici = new bicicleta;
+      $tarjeta->pagar_viaje($bici, "03/11/2017 08:54");
+      $tarjeta->pagar_viaje($bici, "03/11/2017 08:57");
+      $this->assertEquals($tarjeta->get_saldo(), 50 - (12.45 * 2));
+    }
 }
 
 ?>
