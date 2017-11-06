43240e853fd5d40c40d20d075f5ccdbe3dd02128
mgrimblat <mgrimblat@live.com>
2017-11-05 22:55:53 -0300

    update

--- src/colectivo.php
+++ src/colectivo.php
@@ -4,23 +4,19 @@ namespace TpFinal;
 
 class colectivo extends transporte{
 
-    protected $linea;
-    protected $empresa;
+  protected $linea;
+  protected $empresa;
 
-    function __construct($linea, $empresa){
+  function __construct($linea, $empresa){
 
-      $this->linea = $linea;
-      $this->empresa = $empresa;
-      $this->monto = 9.70;
-      $this->tipo = "Colectivo";
-    }
-    
-    public function get_linea(){
-        return $this->linea;
-    }
-    
-    public function get_empresa(){
-        return $this->empresa;
-    }
+    $this->linea = $linea;
+    $this->empresa = $empresa;
+    $this->monto = 9.70;
+    $this->tipo = "Colectivo";
+  }
+
+  public function get_linea(){
+    return $this->linea;
+  }
 }
 ?>
